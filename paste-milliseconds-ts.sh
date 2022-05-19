eac3b1953be504617cbaee099e86f54dca07fcf4
Wei Mo <miloweimo@outlook.com>
2022-05-19 14:47:40 +0800

    ts
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

---
+++ paste-milliseconds-ts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# NOTE: need install gdate (brew install coreutils), refer to: https://stackoverflow.com/questions/9804966/date-command-does-not-follow-linux-specifications-mac-os-x-lion
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ts : Paste Current Millisecond ts Unix Timestamp
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.author Your Name
+# @raycast.authorURL https://github.com/miloweimo
+# @raycast.description Paste Current Millisecond Unix Timestamp.
+# @raycast.packageName Developer Utils
+# @raycast.icon ⏱️
+
+TS=($(($(gdate +%s%N)/1000000)))
+echo -n "$TS" | pbcopy
+osascript -e "tell application \"System Events\" to keystroke \"v\" using command down"
+echo "Copy timestamp:$TS to clipboard !!!"
